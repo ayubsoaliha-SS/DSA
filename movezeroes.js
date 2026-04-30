7779e624c072559c2d8c7cbd1db93c287df49e52
Soaliha Ayub <ayubsoaliha125@gmail.com>
2026-04-30 20:43:11 +0530

    DSA-10

--- movezeroes.js
+++ movezeroes.js
@@ -9,3 +9,18 @@
 // Example 2:
 // Input: nums = [0]
 // Output: [0]
+var moveZeroes = function(nums) {
+    let i = 0; // position to place next non-zero
+
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== 0) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+
+    // fill remaining with 0
+    for (let k = i; k < nums.length; k++) {
+        nums[k] = 0;
+    }
+};
